241079691cb3d2219a901740efa39f4e6980d8c5
sgmt <s.shigematsu17@gmail.com>
2025-07-06 15:21:33 +0900

    難易度選択セレクトボックスの表示

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,38 @@ class WordQuiz {
     }
 
     async init() {
+        await this.fetchQuizData();
+        this.displayStartView();
+    }
+
+    async fetchQuizData() {
         try {
             // クイズデータを非同期で読み込む
             const response = await fetch('./assets/data/quiz.json');
             this.quizData = await response.json();
+            console.log("クイズデータの読み込みに成功しました:", this.quizData);
         } catch (error) {
             console.error("初期化中にエラーが発生しました:", error);
         }
+    }
+
+    displayStartView(){
+        const levelStrs = Object.keys(this.quizData);
+        const optionStrs = [];
+        for (const levelStr of levelStrs) {
+            optionStrs.push(`<option value="${levelStr}">${levelStr}</option>`);
+        }
+        const html = `
+            <h1>クイズゲーム</h1>
+            <p>レベルを選択してください:</p>
+            <select class="level-selector">
+                ${optionStrs.join('')}
+            </select>
+        `;
+        const parentElm = document.createElement('div');
+        parentElm.innerHTML = html;
+
+        this.rootElm.appendChild(parentElm);
     }
 }
 
