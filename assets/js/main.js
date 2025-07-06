7135b91ec7bdd3e067a8c1c319048f7662c02abf
sgmt <s.shigematsu17@gmail.com>
2025-07-06 14:55:09 +0900

    クイズデータを非同期で読み込む機能を追加し、quiz.jsonファイルを作成

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,18 @@ class WordQuiz {
     constructor(){
         console.log("WordQuizクラスのインスタンスが作成されました");
     }
+
+    async init() {
+        try {
+            // クイズデータを非同期で読み込む
+            const response = await fetch('./assets/data/quiz.json');
+            this.quizDara = await response.json();
+            console.log("クイズデータが正常に読み込まれました:", this.quizDara);
+        } catch (error) {
+            console.error("初期化中にエラーが発生しました:", error);
+        }
+        
+    }
 }
 
-new WordQuiz();
+new WordQuiz().init();
